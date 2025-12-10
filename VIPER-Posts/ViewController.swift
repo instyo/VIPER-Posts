efbee978ef559ccc3f7b05ee0bd4c65118a45897
instyo <nsetyo19@gmail.com>
2025-12-10 08:23:51 +0700

    Initial Commit

---
+++ VIPER-Posts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER-Posts
+//
+//  Created by ikhwan on 10/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
