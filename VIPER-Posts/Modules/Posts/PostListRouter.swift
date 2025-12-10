0f174489a774bd645e93890e3692d394930d60df
ikhwan <nsetyo19@gmail.com>
2025-12-10 10:51:52 +0700

    remove builder, initialize module on router

--- VIPER-Posts/Modules/Posts/PostListRouter.swift
+++ VIPER-Posts/Modules/Posts/PostListRouter.swift
@@ -11,4 +11,22 @@ protocol PostListRouterInput {}
 
 class PostListRouter: PostListRouterInput {
     weak var viewController: UIViewController?
+    
+    static func createModule() -> UIViewController {
+        let repository = PostRepositoryImpl.shared
+        let view = PostListViewController()
+        let presenter = PostListPresenter()
+        let interactor = PostListInteractor(repository: repository)
+        let router = PostListRouter()
+        
+        view.presenter = presenter
+        
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.router = router
+        interactor.output = presenter
+        router.viewController = view
+        
+        return UINavigationController(rootViewController: view)
+    }
 }
--- VIPER-Posts/SceneDelegate.swift
+++ VIPER-Posts/SceneDelegate.swift
@@ -18,9 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let repository = PostRepositoryImpl.shared
-        let root = PostListBuilder.build(repository: repository)
-        window?.rootViewController = UINavigationController(rootViewController: root)
+        let root = PostListRouter.createModule()
+        window?.rootViewController = root
         window?.makeKeyAndVisible()
     }
 
